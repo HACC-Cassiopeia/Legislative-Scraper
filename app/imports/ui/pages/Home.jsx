543c9c7b1e4a3078fbb71881b9e3c38d8cc575dc
Shun <saitoshin2145@gmail.com>
2022-10-19 14:49:48 +0900

    issue-07 change the home page

--- app/imports/ui/pages/Home.jsx
+++ app/imports/ui/pages/Home.jsx
@@ -1,12 +1,11 @@
 import React, { useState } from 'react';
-import { Container } from 'react-bootstrap';
+import { Card, Row, Col, Button } from 'react-bootstrap';
 import SideNavBar from '../components/SideNavBar';
-import BillResolutionDetails from '../components/BillResolutionDetails';
 const Home = () => {
   const [isMobile, setIsMobile] = useState(false);
   const mainBodyStyle = {
-    marginLeft: '15%',
-    maxWidth: '85%',
+    marginLeft: '18%',
+    maxWidth: '85vw',
   };
 
   return (
@@ -21,6 +20,33 @@ const Home = () => {
           Legislative Tracking System
         </h2>
         <hr />
+        <Row>
+          <Col>
+            <Card>
+              <Card.Header>PROFILE</Card.Header>
+              <Card.Body>
+                <Card.Title>Name:</Card.Title>
+                <Card.Subtitle>Email:</Card.Subtitle>
+                <Card.Text>
+                  <div style={{ textAlign: 'center' }}>
+                    <b>Role</b>
+                    <br />
+                    <hr />
+                    <Button>EDIT PROFILE</Button>
+                  </div>
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col>
+            <Card>
+              <Card.Header>Upcoming Hearing(s)</Card.Header>
+              <Card.Body>
+                The list of upcoming hearings. A mini dashboard.
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
       </div>
     </div>
   );
