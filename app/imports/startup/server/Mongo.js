f2a7baaf56cf13494ba07794116cbab6e239ef56
acatarinaoaraujo <acoa_@hotmail.com>
2022-10-18 21:21:23 -1000

    Schema for testimonies

--- app/imports/startup/server/Mongo.js
+++ app/imports/startup/server/Mongo.js
@@ -1,6 +1,7 @@
 import { Meteor } from 'meteor/meteor';
 import { Stuffs } from '../../api/stuff/Stuff.js';
 import { FakeData } from '../../api/fakeData/FakeData.js';
+import { Testimonies } from '../../api/testimony/Testimony';
 
 /* eslint-disable no-console */
 
@@ -16,6 +17,12 @@ const addFakeData = (data) => {
   FakeData.collection.insert(data);
 };
 
+// Initialize the database with a default testimonies.
+const addTestimoniesData = (data) => {
+  console.log(`Adding: ${JSON.stringify(data)}`);
+  Testimonies.collection.insert(data);
+};
+
 // Initialize the StuffsCollection if empty.
 if (Stuffs.collection.find().count() === 0) {
   if (Meteor.settings.defaultData) {
@@ -28,4 +35,12 @@ if (Stuffs.collection.find().count() === 0) {
 if (Meteor.settings.fakeData) {
   console.log('Creating fake data database');
   Meteor.settings.fakeData.forEach(data => addFakeData(data));
+}
+
+// Initialize the TestimoniesCollection if empty.
+if (Testimonies.collection.find().count() === 0) {
+  if (Meteor.settings.defaultData) {
+    console.log('Creating default testimony data.');
+    Meteor.settings.defaultData.forEach(data => addTestimoniesData(data));
+  }
 }
