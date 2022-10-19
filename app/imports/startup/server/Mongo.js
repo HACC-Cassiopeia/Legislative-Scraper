4d7efe7f3536c490314e4bcadf21c74936347d59
Giorgio <tran.giorgio@gmail.com>
2022-10-18 14:36:41 -1000

    Add measures to backend
    Created measures schema. Added it to Mongo.js and Publications.js. Tweaked ListStuff.jsx to console.log the measures.

--- app/imports/startup/server/Mongo.js
+++ app/imports/startup/server/Mongo.js
@@ -1,19 +1,24 @@
 import { Meteor } from 'meteor/meteor';
 import { Stuffs } from '../../api/stuff/Stuff.js';
 import { FakeData } from '../../api/fakeData/FakeData.js';
+import { Measures } from '../../api/measures/Measures.js';
 
 /* eslint-disable no-console */
-
 // Initialize the database with a default data document.
 const addData = (data) => {
-  console.log(`  Adding: ${data.name} (${data.owner})`);
+  console.log(`Adding: ${data.name} (${data.owner})`);
   Stuffs.collection.insert(data);
 };
 
 // copied add fake data
 const addFakeData = (data) => {
-  console.log(`Adding: ${JSON.stringify(data)}`);
+  console.log('Added fake data');
   FakeData.collection.insert(data);
+};
+
+const addMeasures = (measure) => {
+  console.log(`Adding: ${measure.code}`);
+  Measures.collection.insert(measure);
 };
 
 // Initialize the StuffsCollection if empty.
@@ -28,4 +33,12 @@ if (Stuffs.collection.find().count() === 0) {
 if (Meteor.settings.fakeData) {
   console.log('Creating fake data database');
   Meteor.settings.fakeData.forEach(data => addFakeData(data));
+}
+
+// Initialization of test Measures
+if (Measures.collection.find().count() === 0) {
+  if (Meteor.settings.measures) {
+    console.log('Creating measures database');
+    Meteor.settings.measures.forEach(measure => addMeasures(measure));
+  }
 }
