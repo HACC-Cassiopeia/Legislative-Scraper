14339b32a209fe47fb579e4ec3ef3038fc218408
Giorgio <tran.giorgio@gmail.com>
2022-10-20 22:31:38 -1000

    Reformat scraper into frontend util

---
+++ app/imports/ui/utils/Scraper.js
@@ -0,0 +1,80 @@
+import axios from 'axios';
+import cheerio from 'cheerio';
+
+const scrapeData = (year, measureType) => {
+  const url = `https://www.capitol.hawaii.gov/advreports/advreport.aspx?year=
+  ${year}&report=deadline&active=true&rpt_type=&measuretype=${measureType}`;
+  const data = [];
+
+  axios(url)
+    .then(response => {
+      const html = response.data;
+      const $ = cheerio.load(html);
+      let index = 0;
+      // if it's less than 10, add a zero in front to conform with format
+      const getIndex = (num) => (num < 10 ? `0${index}` : index);
+
+      $('tr', html).each(function () {
+        index += 1;
+
+        const code = $(this)
+          .find(`#GridViewReports_ctl${getIndex(index)}_HyperLink1`)
+          .text();
+        const measurePdfUrl = $(this)
+          .find(`#GridViewReports_ctl${getIndex(index)}_HyperLink2`)
+          .attr('href');
+        const measureArchiveUrl = $(this)
+          .find(`#GridViewReports_ctl${getIndex(index)}_HyperLink1`)
+          .attr('href');
+        const measureTitle = $(this)
+          .find(`#GridViewReports_ctl${getIndex(index)}_Label7`)
+          .text();
+        const reportTitle = $(this)
+          .find(`#GridViewReports_ctl${getIndex(index)}_Label1`)
+          .text();
+        const description = $(this)
+          .find(`#GridViewReports_ctl${getIndex(index)}_Label2`)
+          .text();
+        const statusHorS = $(this)
+          .find(`#GridViewReports_ctl${getIndex(index)}_Label3`)
+          .text();
+        const statusDescription = $(this)
+          .find(`#GridViewReports_ctl${getIndex(index)}_Label4`)
+          .text();
+        const statusDate = $(this)
+          .find(`#GridViewReports_ctl${getIndex(index)}_Label5`)
+          .text();
+        const introducer = $(this)
+          .find(`#GridViewReports_ctl${getIndex(index)}_Label27`)
+          .text();
+        const currentReferral = $(this)
+          .find(`#GridViewReports_ctl${getIndex(index)}_Label9`)
+          .text();
+        const companion = $(this)
+          .find(`#GridViewReports_ctl${getIndex(index)}_Label6`)
+          .find('a')
+          .text();
+
+        if (`${code}`.length !== 0) {
+          data.push({
+            code: `${code}`,
+            measurePdfUrl: `${measurePdfUrl}`,
+            measureArchiveUrl: `${measureArchiveUrl}`,
+            measureTitle: `${measureTitle}`,
+            reportTitle: `${reportTitle}`,
+            description: `${description}`,
+            statusHorS: `${statusHorS}`,
+            statusDescription: `${statusDescription}`,
+            statusDate: `${statusDate}`,
+            introducer: `${introducer}`,
+            currentReferral: `${currentReferral}`,
+            companion: `${companion}`,
+          });
+        }
+      });
+      // eslint-disable-next-line no-console
+    }).catch(() => console.log('Failed, invalid input'));
+  return data;
+};
+
+export default scrapeData();
