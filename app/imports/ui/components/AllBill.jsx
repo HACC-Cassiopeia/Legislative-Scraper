9b668c27788f0deff7d8181e91d85a5f6d98b420
Rob Godfrey <102856686+robertgodfrey@users.noreply.github.com>
2022-10-22 10:47:03 -1000

    Removed bill description from all-dashboard

--- app/imports/ui/components/AllBill.jsx
+++ app/imports/ui/components/AllBill.jsx
@@ -5,32 +5,6 @@ import { Star, StarFill } from 'react-bootstrap-icons';
 import { Accordion, Button } from 'react-bootstrap';
 
 const selected = false; // TODO will need to load from db
-const minDescLength = 100;
-const maxDescLength = 120;
-
-// shorter bill description
-function getShortDesc(desc) {
-  let sub = minDescLength;
-  while (sub < maxDescLength) {
-    if (desc[sub] === ' ') {
-      break;
-    }
-    sub++;
-  }
-  return `${desc.substring(0, sub)}...`;
-}
-
-// the rest of the bill description
-function getRemainingDesc(desc) {
-  let sub = minDescLength;
-  while (sub < maxDescLength) {
-    if (desc[sub] === ' ') {
-      break;
-    }
-    sub++;
-  }
-  return desc.substring(sub + 1);
-}
 
 /** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
 const AllBill = ({ bill }) => (
@@ -42,23 +16,20 @@ const AllBill = ({ bill }) => (
     </td>
     <td>
       <div style={{ fontSize: '20px' }}><Link to={`/view/${bill.code}`}><strong>{bill.code}</strong></Link></div>
-      {bill.measureTitle} <br />
       <Accordion flush className="billAccordion">
         <Accordion.Item eventKey="0">
-          <Accordion.Header> {getShortDesc(bill.description)} </Accordion.Header>
-          <Accordion.Body>
-            {getRemainingDesc(bill.description)}
-          </Accordion.Body>
+          <Accordion.Header>{bill.measureTitle} </Accordion.Header>
+          <Accordion.Body>{bill.description}</Accordion.Body>
         </Accordion.Item>
       </Accordion>
     </td>
-    <td>**offices assigned**</td>
-    <td>**action to be taken**</td>
+    <td>*offices assigned*</td>
+    <td>*action to be taken*</td>
     <td>{bill.currentReferral}</td>
-    <td>**hearing date/time**</td>
-    <td>**DOE position**</td>
-    <td>**testifier**</td>
-    <td>**internal status**</td> {/* THIS IS THE INTERNALLY TRACKED DOE STATUS, NOT THE STATUS ON THE STATE WEBSITE */}
+    <td>*hearing date/time*</td>
+    <td>*DOE position*</td>
+    <td>*testifier*</td>
+    <td>*internal status*</td> {/* THIS IS AN INTERNALLY TRACKED DOE STATUS, NOT THE STATUS ON THE STATE WEBSITE */}
   </tr>
 );
 
